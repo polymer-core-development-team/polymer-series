55cef69c77bc919d8e180abb96e7f5dc4839ad46
WarmthDawn <warmth06@sina.com>
2022-01-30 15:32:54 +0800

    :see_no_evil: 添加初始化脚本和.gitignore

---
+++ setup_workspace.bat
@@ -0,0 +1,7 @@
+git clone https://github.com/teampolymer/polymer-core.git
+git clone https://github.com/teampolymer/polymer-hinge.git
+git clone https://github.com/teampolymer/polymer-schematic.git
+git clone https://github.com/teampolymer/polymer-scripts.git
+git clone https://github.com/teampolymer/polymer-gui.git
+git clone https://github.com/teampolymer/polymer-components.git
+git clone https://github.com/teampolymer/polymer-machine.git
